92c5132f509cdea0ea0f3d44eab22bebbfd0f38d
ZingP <605156398@qq.com>
2017-08-03 09:48:52 +0800

    递归和数组

--- src/go_dev/day04/数组/main/main.go
+++ src/go_dev/day04/数组/main/main.go
@@ -1,4 +1,7 @@
 package main
+
+import "fmt"
+
 /*
 数组：同一种数据类型的固定长度的序列
 定义： var a [len]int 比如var a [5]int
@@ -7,4 +10,18 @@ package main
 访问越界：访问下标在数组下标范围外，会触发越界，会panic
 数组是值类型，因此改变副本的值，不会改变本身的值。
 */
+
+func main() {
+	var a [10]int
+	a[0] = 10
+	a[1] = 100
+
+	for i:=0;i<len(a);i++{
+		fmt.Println(a[i])
+	}
+
+	for index,v := range a {
+		fmt.Printf("a[%d] = %d, ", index, v)
+	}
+}
 

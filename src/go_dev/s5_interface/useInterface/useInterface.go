8495aaf295f09aba9cf7d20142d5f57333bccb66
ZingP <605156398@qq.com>
2018-03-13 22:13:07 +0800

    go socket

--- src/go_dev/s5_interface/useInterface/useInterface.go
+++ src/go_dev/s5_interface/useInterface/useInterface.go
@@ -9,7 +9,7 @@ import "fmt"
 // notifier 是一个定义了
 // 通知类行为的接口
 type notifier interface {
-notify()
+	notify()
 }
 
 // user 在程序里定义一个用户类型
@@ -32,7 +32,7 @@ func main() {
 
 // ./listing36.go:32: 不能将 u（类型是 user）作为
 // sendNotification 的参数类型 notifier：
- // user 类型并没有实现 notifier
+// user 类型并没有实现 notifier
 // （notify 方法使用指针接收者声明）
 }
 

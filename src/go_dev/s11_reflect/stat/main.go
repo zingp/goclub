60ab77425e7ec17c5e7b2f42284fb1e19fa6fb84
ZingP <605156398@qq.com>
2018-07-11 12:00:37 +0800

    标准输入输出

--- src/go_dev/s11_reflect/stat/main.go
+++ src/go_dev/s11_reflect/stat/main.go
@@ -15,6 +15,11 @@ func (p *People)TalkSelf(){
 	fmt.Printf("My name is %s", p.Name)
 }
 
+func (p People)SetAge(a int)People{
+	p.Age = a
+	return p
+}
+
 //  reflect.TypeOf()
 func testTypeOf(a interface{}) {
 
@@ -42,20 +47,52 @@ func testNumMethod(a interface{}) {
 	fmt.Println("method nums:", n)
 }
 
+func testMethodByName(a interface{}) {
+	v := reflect.ValueOf(a)
+	r := v.MethodByName("TalkSelf")
+	fmt.Println(r)
+
+	// var b []reflect.Value
+	// b = append(b,)
+	r.Call(nil)
+
+}
+
+func testElemCall(a interface{}) {
+	v := reflect.ValueOf(a)
+	r := v.MethodByName("SetAge")
+	fmt.Println(r)
+
+	var b []reflect.Value
+	age := 99
+	newAge := reflect.ValueOf(age)
+	b = append(b, newAge)
+
+	p := r.Call(b)     // 返回的是一个切片
+	fmt.Println(p)
+	fmt.Println(p[0])
+}
 func main() {
 	var p = People {
 		Name: "liu",
 		Age: 23,
 	}
-	// testTypeOf(p)
-	// testValueOf(p)
+	//  testTypeOf(p)
+	//  testValueOf(p)
 
-	// var name = "zingp"
-	// testTypeOf(name)
-	// testValueOf(name)
+	//  var name = "zingp"
+	//  testTypeOf(name)
+	//  testValueOf(name)
 
-	// testNumField(p)
+	//  testNumField(p)
 	
-	testNumMethod(p)    // 对象其实并没有实现方法，所以输出结果是0
-	testNumMethod(&p)   // 指针类型实现了一个方法，所以输出结果是1
+	// testNumMethod(p)    // 对象其实并没有实现方法，所以输出结果是0
+	// testNumMethod(&p)   // 指针类型实现了一个方法，所以输出结果是1
+
+	// testMethodByName(&p)
+	// p2 := p.SetAge(100)
+	// fmt.Println(p2)
+
+	testElemCall(p)
+
 }

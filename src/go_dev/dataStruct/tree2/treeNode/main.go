a46fd7408f6bc9064630cd9806c241ba661df537
zingp <605156398@qq.com>
2018-07-30 17:45:07 +0800

    4 func about binary tree traversql

--- src/go_dev/dataStruct/tree2/treeNode/main.go
+++ src/go_dev/dataStruct/tree2/treeNode/main.go
@@ -6,7 +6,7 @@ import (
 
 // Node is a representation of binary tree node
 type Node struct {
-	value int
+	value interface{}
 	left  *Node
 	right *Node
 }
@@ -25,42 +25,84 @@ func gen2Tree() (node *Node) {
 }
 
 // 广度优先
-func breadthFirstSearch(node Node) []int {
-	var result []int
+func breadthFirstSearch(node Node) []interface{} {
+	var result []interface{}
 	nodes := []Node{node}
 
 	for len(nodes) > 0 {
 		node := nodes[0]
-		nodes = nodes[1:]  // nodes 切片缩小，不断向后推移
+		nodes = nodes[1:] // nodes 切片缩小，不断向后推移
 		result = append(result, node.value)
-		if (node.left != nil) {
+		if node.left != nil {
 			nodes = append(nodes, *node.left)
 		}
-		if (node.right != nil) {
+		if node.right != nil {
 			nodes = append(nodes, *node.right)
 		}
 	}
 	return result
 }
 
-// 前序遍历
-func preOrderTraverse(node *Node) {
-
-	fmt.Println(node.value)
+// 前序遍历  根->左子树->右子树
+func preOrderTraversal(node *Node) {
+	fmt.Printf(" %v", node.value)
 	if node.left != nil {
-		preOrderTraverse(node.left)
+		preOrderTraversal(node.left)
 	}
 	if node.right != nil {
-		preOrderTraverse(node.right)
+		preOrderTraversal(node.right)
 	}
 }
 
+// 中序遍历 左子树 根 右子树
+func midOrderTraversal(node *Node) {
+	if node.left != nil {
+		midOrderTraversal(node.left)
+	}
+	fmt.Printf(" %v", node.value)
+	if node.right != nil {
+		midOrderTraversal(node.right)
+	}
+}
+
+// 后序遍历
+func postOrderTraversal(node *Node) {
+	if node.left != nil {
+		postOrderTraversal(node.left)
+	}
+	if node.right != nil {
+		postOrderTraversal(node.right)
+	}
+	fmt.Printf(" %v", node.value)
+}
+
+/*  // 非递归遍历有问题
+func inOderTravelsal1(node *Node) {
+	n := node
+	stack := NewStack()
+	for n != nil || stack.Size() != 0 {
+		for n != nil {
+			stack.Push(n)
+			n = n.left
+		}
+		if stack.Size() != 0 {
+			t := stack.Pop()
+			fmt.Printf(" %v", t.(*Node))
+			n = n.right
+		}
+	}
+}
+*/
+
 func main() {
 	root := gen2Tree()
-	preOrderTraverse(root)
+	preOrderTraversal(root)
+	fmt.Println()
+	midOrderTraversal(root)
+	fmt.Println()
+	postOrderTraversal(root)
+	fmt.Println()
 
 	array := breadthFirstSearch(*root)
-	for _, i := range array {
-		fmt.Println(i)
-	}
+	fmt.Println(array)
 }

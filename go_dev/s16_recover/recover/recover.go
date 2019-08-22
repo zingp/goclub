1f6c5a28ed89aebf74d4cb66437fc580528bfb97
zingp <605156398@qq.com>
2019-08-22 16:02:01 +0800

    recover

--- go_dev/s16_recover/recover/recover.go
+++ go_dev/s16_recover/recover/recover.go
@@ -2,26 +2,27 @@ package main
 
 import(
 	"fmt"
+	// "runtime/debug"
 )
 
-func set(p *int){
-	*p = 100
-}
 
-func testRecover(){
-	// recover 捕获异常，防止程序挂掉；defer 要写在前面
-	defer func(){
-		err := recover()
-		if err != nil {
-			fmt.Printf("Panic:%v\n", err)
-		}
-	}()
-
-	// 空指针
-	var p *int
-	set(p)
+func myfunc() {
+	fmt.Println("a")
+    panic(55)
+    fmt.Println("b")
+	fmt.Println("f")
 }
 
 func main(){
-	testRecover()
+	// recover 捕获异常，防止程序挂掉；defer 要写在前面
+	defer func(){
+
+		if err := recover(); err != nil {
+			fmt.Printf("Panic:%v\n", err)
+		} else {
+			fmt.Println("process exited normally with status 0.")
+		}
+	}()
+
+	myfunc()
 }

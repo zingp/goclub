35dc4920c742cfe45306ff0c7a728a5b3c773c58
zingp <605156398@qq.com>
2020-03-21 22:33:08 +0800

    提取预训练词向量,写入文件

--- p040词向量删选/buildembed.go
+++ p040词向量删选/buildembed.go
@@ -7,6 +7,14 @@ import (
 	"bufio"
 	"strings"
 )
+/*
+[1] 读取词表文件
+[2] 读取embeddings文件
+[3] 过滤
+UNK 的词向量表示？
+PAd 的词向量如何表示？
+*/
+
 func LoadEmbedding(f string) map[string][]string{
 	embedings := map[string][]string{}
 	file, err := os.Open(f)
@@ -39,8 +47,65 @@ func LoadEmbedding(f string) map[string][]string{
 	return embedings
 }
 
+
+func GetWordEmbeds(f string, m map[string][]string) map[string][]string {
+	embedings := map[string][]string{}
+
+	file, err := os.Open(f)
+	if err != nil {
+		fmt.Println("open file err:", err)
+		return embedings
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("read string err", err)
+		}
+		sliceLine := strings.Split(line, " ")
+		word := sliceLine[0]
+		// id := sliceLine[1]
+
+		if vec, ok := m[word]; ok {
+			embedings[word] = vec
+		} 
+
+	}	
+	return embedings
+}
+
+func WriteMap(file string, m map[string][]string) {
+	f, err := os.Create(file) 
+	if err != nil {
+		fmt.Println("open file err:", err)
+		return 
+	}
+	defer f.Close()
+
+	w := bufio.NewWriter(f)  //创建新的 Writer 对象
+	for word, vec := range m {
+		vecString := strings.Join(vec, " ")
+		str := fmt.Sprintf("%s\t%s", word, vecString)
+		_, err := w.WriteString(str)
+		if err != nil {
+			fmt.Println("write file err:", err)
+			return 
+		}
+	}
+	w.Flush()
+}
+
 func main() {
 	filename := "/Users/liuyouyuan/Documents/sgns.weibo.char"
-	embedings := LoadEmbedding(filename)
-	fmt.Println(len(embedings))
+	vocabFile := "./vocab.txt"
+	newEmbedFile := "./wordvec.txt"
+	pretraineEmbeds := LoadEmbedding(filename)
+	newEmbeds := GetWordEmbeds(vocabFile, pretraineEmbeds)
+	WriteMap(newEmbedFile, newEmbeds)
+	fmt.Println("Done!")
 }

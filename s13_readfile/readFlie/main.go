1960f679c3161210b817bf46e47d1b956340d0d8
zingp <605156398@qq.com>
2020-04-17 12:54:28 +0800

    读文件

--- s13_readfile/readFlie/main.go
+++ s13_readfile/readFlie/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"bufio"
 	"io/ioutil"
+	"time"
 )
 
 //方法一
@@ -46,16 +47,17 @@ func ReadFlieBufio(d string) {
 
 	reader := bufio.NewReader(file)
 	for {
-		line, err := reader.ReadString('\n')
+		line, err := reader.ReadString('\n')	
 		if err == io.EOF {
-			break
+			fmt.Println(line)
+			return
 		}
 		if err != nil {
-			fmt.Println("read string err", err)
+			fmt.Println("read line err", err)
+			continue
 		}
-		fmt.Println(line)
+		//fmt.Println(line)
 	}
-
 }
 
 //方法三:一次性读取整个文件，别用来读取大文件
@@ -68,10 +70,59 @@ func testIoutilReadFile(d string) {
 
 }
 
+// 按照行读，ReadLine会更快
+func bufioReadLine(d string) {
+	file, err := os.Open(d)
+	if err != nil {
+		fmt.Println("open file err:", err)
+		return
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	count := 0
+	for {
+		//line, err := reader.ReadString('\n')
+		line, isprefix, err := reader.ReadLine()
+		for isprefix && err == nil {
+			var bs []byte
+			bs, isprefix, err = reader.ReadLine()
+			line = append(line, bs...)
+		}
+		if err == io.EOF {
+			if len(line) == 0 {
+				return
+			}
+			fmt.Println(line, isprefix)
+			return
+		}
+		if err != nil {
+			fmt.Println("read line err", err)
+			continue
+		}
+		//fmt.Println(line)
+		count += 1
+		if count % 2000000 == 0 {
+			fmt.Println(string(line), isprefix)	
+		}
+	}
+}
+
+func durationTime(start int64, t string) int64 {
+	end := time.Now().UnixNano()
+	if t == "ms" {
+		return (end - start) / int64(time.Millisecond)
+	}
+	return (end - start) / int64(time.Second)
+}
+
 
 func main() {
-	var dir string = "C:/Users/liuyouyuan/Desktop/笔记/部署文档/centos6下go语言环境搭建.txt"
+	file := os.Args[1]
+	fmt.Println("file:", file)
+	start := time.Now().UnixNano()
+	ReadFlieBufio(file)
+	fmt.Printf("Duration time %d ms.\n", durationTime(start, "ms"))
 	//ReadFile(dir)
-	//ReadFlieBufio(dir)
-	testIoutilReadFile(dir)
+	// testIoutilReadFile(dir)
 }

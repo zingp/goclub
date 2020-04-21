93ea6125907f80058c315e2f21cef6243bd2d6cd
zingp <605156398@qq.com>
2020-04-21 18:43:07 +0800

    strinscs

--- P045strings/stringscs.go
+++ P045strings/stringscs.go
@@ -9,9 +9,12 @@ func main() {
 	line := "我们 0.242507 -0.144510 0.401783 -0.471121 "
 	// 返回子串sep在字符串s中第一次出现的索引值，不在的话返回-1.
 	idx := strings.Index(line, " ")
-    // chars中任何一个Unicode代码点在s中首次出现的位置，不存在返回-1
+	// chars中任何一个Unicode代码点在s中首次出现的位置，不存在返回-1
 	idx2 := strings.IndexAny(line, " ")
-	
+
 	fmt.Println(line[:idx2], idx2)
 	fmt.Println(idx)
+
+	s := "AG 输了"
+	fmt.Println(strings.ToLower(s))
 }
